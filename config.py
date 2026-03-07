b838e448d0d24b1a2668d3a355481c0cc7a575e4
刘湘政 <1297289148@qq.com>
2026-03-07 22:05:03 +0800

    doc: "正式版本1.0"

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 PORT = int(os.environ.get("PORT", 3100))
 DB_PATH = os.environ.get("DB_PATH", "data/llm_dispatcher.db")
-UPSTREAM_TIMEOUT = int(os.environ.get("UPSTREAM_TIMEOUT", 15))
+UPSTREAM_TIMEOUT = int(os.environ.get("UPSTREAM_TIMEOUT", 60))
 MAX_FAILOVER_ATTEMPTS = 10
 DEBUG = 1
 Cooldown = 300  # s
